6dea020415cba3c38ffa09ffc49ce086576b2d6e
Mike Solomon <msolomon@thrillist.com>
2016-04-05 20:54:38 -0400

    tabs to spaces

--- app.rb
+++ app.rb
@@ -6,14 +6,14 @@ require './jira_helpers'
 require './github_helpers'
 
 post '/payload' do
-	#the type of event that happened in GitHub
-	event = request.env["HTTP_X_GITHUB_EVENT"]
-	#the JSON that GitHub webhook sends us
-	push = JSON.parse(request.body.read)
-	#if the event was a pull request, handle that differently than actions for branches
-	if event == "pull_request"
-		handle_github_pull_request push
-	elsif event == "create" && push["ref_type"] == "branch"
+  #the type of event that happened in GitHub
+  event = request.env["HTTP_X_GITHUB_EVENT"]
+  #the JSON that GitHub webhook sends us
+  push = JSON.parse(request.body.read)
+  #if the event was a pull request, handle that differently than actions for branches
+  if event == "pull_request"
+    handle_github_pull_request push
+  elsif event == "create" && push["ref_type"] == "branch"
     handle_github_branch push
-	end
+  end
 end
--- github_helpers.rb
+++ github_helpers.rb
@@ -7,7 +7,7 @@ def handle_github_branch (push)
   branch = push["ref"]
   #the user who made the action to the pull request
   github_user = get_github_data push["sender"]["url"]
-	user = translate_github_user_to_jira_user github_user
+  user = translate_github_user_to_jira_user github_user
   #update the JIRA issue with the branch name
   jira_issues = get_jira_issues branch, "branch"
   #update_development_info_jira jira_issues, branch, "branch"
@@ -15,54 +15,54 @@ def handle_github_branch (push)
 end
 
 def handle_github_pull_request (push)
-	#the action that was taken
-	action = push["action"]
-	#the user who made the action to the pull request
+  #the action that was taken
+  action = push["action"]
+  #the user who made the action to the pull request
   github_user = get_github_data push["sender"]["url"]
-	user = translate_github_user_to_jira_user github_user
-	#the pull request that was actioned on
-	pull_request = push["pull_request"]
-	#jira issues associated with the pull request
-	jira_issues = get_jira_issues pull_request, "pull_request"
+  user = translate_github_user_to_jira_user github_user
+  #the pull request that was actioned on
+  pull_request = push["pull_request"]
+  #jira issues associated with the pull request
+  jira_issues = get_jira_issues pull_request, "pull_request"
 
-	if action == "labeled"
-		#array of labels applied to this pull request
-		pull_request_labels = get_labels pull_request
-		#the label that was just added to this pull request
-		current_label = push["label"]["name"]
-		#loop through all of the tickets and decide what to do based on the labels of this pull request
-		update_label_jira jira_issues, current_label, pull_request_labels, user
+  if action == "labeled"
+    #array of labels applied to this pull request
+    pull_request_labels = get_labels pull_request
+    #the label that was just added to this pull request
+    current_label = push["label"]["name"]
+    #loop through all of the tickets and decide what to do based on the labels of this pull request
+    update_label_jira jira_issues, current_label, pull_request_labels, user
 
-	elsif action == "synchronize"
-		#get latest commit message on pull request
-		latest_commit_message = get_latest_commit_message pull_request, push["repository"]["commits_url"]
-		#update jira ticket by moving to QA and commenting with the latest commit message
-		update_message_jira jira_issues, pull_request, latest_commit_message, user
+  elsif action == "synchronize"
+    #get latest commit message on pull request
+    latest_commit_message = get_latest_commit_message pull_request, push["repository"]["commits_url"]
+    #update jira ticket by moving to QA and commenting with the latest commit message
+    update_message_jira jira_issues, pull_request, latest_commit_message, user
 
-	elsif action == "opened"
-		start_code_review jira_issues, pull_request, user
-	end
+  elsif action == "opened"
+    start_code_review jira_issues, pull_request, user
+  end
 end
 
 def get_github_data (url)
-	data = JSON.parse( RestClient.get( url, {:params => {:access_token => ENV['GITHUB_TOKEN']}, :accept => :json} ) )
+  data = JSON.parse( RestClient.get( url, {:params => {:access_token => ENV['GITHUB_TOKEN']}, :accept => :json} ) )
 
   return data
 end
 
 #returns an array of labels applied to a pull request
 def get_labels (pull_request)
-	labels_url = pull_request["issue_url"] + "/labels"
+  labels_url = pull_request["issue_url"] + "/labels"
   labels = get_github_data labels_url
 
-	return labels
+  return labels
 end
 
 #returns message of the latest commit for a pull request
 def get_latest_commit_message (pull_request, commits_url)
-	commit_info_url = commits_url.split('{')[0] + '/' + pull_request["head"]["sha"]
-	latest_commit_info = get_github_data commit_info_url
-	latest_commit_message = latest_commit_info["commit"]["message"]
+  commit_info_url = commits_url.split('{')[0] + '/' + pull_request["head"]["sha"]
+  latest_commit_info = get_github_data commit_info_url
+  latest_commit_message = latest_commit_info["commit"]["message"]
 
-	return latest_commit_message
+  return latest_commit_message
 end
